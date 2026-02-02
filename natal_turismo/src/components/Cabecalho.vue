0b984f23e765e2b9c75022c2361b6e54f74b9426
Noltib <joaopedro999897@gmail.com>
2026-02-02 19:49:39 -0300

    comecinho

---
+++ natal_turismo/src/components/Cabecalho.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>
+        <nav>
+            <figure><img src="#" alt=""></figure>  
+            <ul>
+                <li>inicio</li>
+                <li>pontos turisticos</li>
+                <li>restaurantes</li>
+            </ul>
+        </nav>
+    </header>
+</template>
