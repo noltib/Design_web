563365f3db6c3a1c6ac41e588836c494d79333d4
noltib <joaopedro999897@gmail.com>
2026-02-05 14:56:24 +0000

    menu hamburguer

--- natal_turismo/src/components/Cabecalho.vue
+++ natal_turismo/src/components/Cabecalho.vue
@@ -1,17 +1,102 @@
 <template>
   <header>
     <nav>
+      <RouterLink to="/">
+        <img :src="logo" alt="Logo">
+      </RouterLink>
 
+      <button class="hamburger" @click="menuAberto = !menuAberto"> ☰ </button>
 
-      <ul>
-        <li><RouterLink to="/">Início</RouterLink></li>
+      <ul :class="{ ativo: menuAberto }">
+        <li><RouterLink to="/">Pontos turísticos</RouterLink></li>
+        <li><RouterLink to="/">Restaurantes</RouterLink></li>
+        <li><RouterLink to="/">Passeios & Aventuras</RouterLink></li>
+        <li><RouterLink to="/">Curiosidades & Histórias</RouterLink></li>
       </ul>
     </nav>
   </header>
 </template>
 
+
 <script>
+import logo from "@/assets/logo.svg";
+
 export default {
   name: "Cabecalho",
+  data() {
+    return {
+      logo,
+      menuAberto: false,
+    };
+  },
 };
 </script>
+
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  padding: 0;
+  margin: 1em;
+}
+
+ul {
+  list-style-type: none;
+  display: flex;
+  gap: 1rem;
+  background-color: #01A9B0;
+  padding: 1em;
+  border-radius: 10px;
+  margin: 0;
+}
+
+li a {
+  text-decoration: none;
+  padding: 1em;
+  color: white;
+}
+
+li a:hover {
+  background-color: #39b5b8;
+}
+
+img {
+  height: 3em;
+}
+
+.hamburger {
+  display: none;
+  font-size: 2rem;
+  background: none;
+  border: none;
+  color: #ffffff;
+  cursor: pointer;
+  background-color: #01A9B0;
+  border-radius: 5px;
+
+}
+
+
+@media (max-width: 795px) {
+  .hamburger {
+    display: block;
+  }
+
+  ul {
+    display: none;
+    flex-direction: column;
+    position: absolute;
+    top: 100%;
+    right: 0;
+    width: 100%;
+    border-radius: 0;
+  }
+
+  ul.ativo {
+    display: flex;
+  }
+}
+</style>
